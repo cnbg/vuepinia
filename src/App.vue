b0ed71d03a7a35abcad7541a1ad99f2c3436a89f
chyngyz <nurchin@gmail.com>
2024-04-20 19:40:06 +0600

    add tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script setup>
+import Menubar from 'primevue/menubar'
 import ToolBar from '@/components/common/ToolBar.vue'
 import SideBar from '@/components/common/SideBar.vue'
 import AddNotePage from '@/pages/note/AddNotePage.vue'
+
+const items = [
+  {label: 'Home', icon: 'pi mr-2 pi-home', link: '/home'},
+  {label: 'Home', icon: 'pi mr-2 pi-home', link: '/home'},
+  {label: 'Home', icon: 'pi mr-2 pi-home', link: '/home'},
+]
 </script>
 
 <template>
   <div class="rs__notes-wrapper">
+    <Menubar :model="items" />
+    <div class="text-3xl font-bold underline">
+      tailwind works
+    </div>
     <ToolBar />
     <div class="rs__content-wrapper">
       <SideBar />
